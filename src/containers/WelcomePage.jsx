cfbb0d164c50c431d6c4c60f1380ba4aaac4a88e
matteoaricci <matteo.a.ricci@gmail.com>
2020-05-13 10:17:34 -0400

    Tabs working for welcome

--- src/containers/WelcomePage.jsx
+++ src/containers/WelcomePage.jsx
@@ -8,17 +8,34 @@ import Tabs from '@material-ui/core/Tabs'
 import Tab from '@material-ui/core/Tab'
 
 class WelcomePage extends Component {
+    constructor() {
+        super();
+        this.state = {
+            selected: 'Login',
+            value: 0
+        }
+    }
+
+    handleOnChange = (event, value) => {
+        this.setState({value: value})
+    }
     render() {
+       const { selected } = this.state;
+       const { value } = this.state;
         return (
             <div>
                 <Paper>
-                    <Tabs centered value={0}>
+                    <Tabs centered value={value}
+                          onChange={this.handleOnChange}
+                    >
                         <Tab label="Sign In"/>
                         <Tab label="Create New User"/>
                     </Tabs>
                 </Paper>
-                <Login handleLogin={this.props.handleLogin}/>
+                {value === 0 ? 
+                <Login handleLogin={this.props.handleLogin}/> :
                 <SignUp handleLogin={this.props.handleLogin}/>
+                }
             </div>
         );
     }
